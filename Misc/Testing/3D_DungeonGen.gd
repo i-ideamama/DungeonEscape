f1ea3da741958b0c6dd1959006500d8b7d65d113
i-ideamama <advaith.n@gmail.com>
2024-06-07 17:53:55 +0530

    feat: got basic 3d dungeon generation working

--- Misc/Testing/3D_DungeonGen.gd
+++ Misc/Testing/3D_DungeonGen.gd
@@ -9,8 +9,9 @@ var min_room_size = 8
 var max_room_size = 15
 
 var room_pos_list = []
+var room_pos_list_cpy = []
 
-var pos_range = Vector2(-35,35)
+var pos_range = Vector2(-50,50)
 
 var mesh_resolution = 5
 
@@ -23,6 +24,9 @@ func setgenerate(_val:bool) -> void:
 	do_the_gen()
 
 @export var clear : bool = false : set = setclear
+
+@onready var player = get_node("Player")
+
 func setclear(_val:bool) -> void:
 	for c in get_children():
 		c.queue_free()
@@ -30,10 +34,13 @@ func setclear(_val:bool) -> void:
 
 func _ready():
 	randomize()
+	do_the_gen()
+	#player.position = room_pos_list[0]
 
 func do_the_gen():
 	get_room_positions()
 	place_rooms()
+	get_rooms()
 
 func get_room_positions():
 	room_count = randi_range(min_rooms, max_rooms)
@@ -44,9 +51,9 @@ func get_room_positions():
 		while(!can_place(room_pos)):
 			room_pos = Vector3(randi_range(pos_range.x,pos_range.y),0,randi_range(pos_range.x,pos_range.y))
 		room_pos_list.append(room_pos)
-		
-	for p in room_pos_list:
-		print(p)
+	
+	room_pos_list_cpy = room_pos_list
+
 
 func place_rooms():
 	for p in room_pos_list:
@@ -60,8 +67,7 @@ func place_rooms():
 		mesh.material = mat
 		mesh.position = p
 		add_child(mesh)
-		
-		
+
 func can_place(pos):
 	if not room_pos_list:
 		return true
@@ -71,3 +77,59 @@ func can_place(pos):
 			if(dist<max_room_size):
 				return false
 		return true
+
+func get_rooms():
+	var room1
+	var room2
+	while room_pos_list_cpy.size() > 2:
+		room1 = room_pos_list_cpy[randi_range(0, room_pos_list_cpy.size()-1)]
+		room2 = room_pos_list_cpy[randi_range(0, room_pos_list_cpy.size()-1)]
+		
+		while room1==room2:
+			room2 = room_pos_list_cpy[randi_range(0, room_pos_list_cpy.size()-1)]
+			
+		connect_rooms(room1, room2)
+		room_pos_list_cpy.erase(room1)
+
+	if room_pos_list_cpy.size()==2:
+		room1 = room_pos_list_cpy[0]
+		room2 = room_pos_list_cpy[1]
+		connect_rooms(room1, room2)
+	
+func connect_rooms(room1, room2):
+	var dir1 = room2.x - room1.x
+	var dir2 = room2.z - room1.z
+	
+	var mesh1 = CSGMesh3D.new()
+	mesh1.mesh = BoxMesh.new()
+	mesh1.scale = Vector3(abs(dir1)+3, 3, 3)
+	mesh1.mesh.subdivide_width = mesh1.scale.x/mesh_resolution
+	mesh1.mesh.subdivide_height = mesh1.scale.y/mesh_resolution
+	mesh1.mesh.subdivide_depth = mesh1.scale.z/mesh_resolution
+	mesh1.position = room1 + Vector3(dir1/2, 0, 0)
+	mesh1.material = mat
+	mesh1.flip_faces = true
+	add_child(mesh1)
+	
+	var mesh2 = CSGMesh3D.new()
+	mesh2.mesh = BoxMesh.new()
+	mesh2.scale = Vector3(3, 3, abs(dir2)+3)
+	mesh2.mesh.subdivide_width = mesh2.scale.x/mesh_resolution
+	mesh2.mesh.subdivide_height = mesh2.scale.y/mesh_resolution
+	mesh2.mesh.subdivide_depth = mesh2.scale.z/mesh_resolution
+	mesh2.position = room2 - Vector3(0, 0, dir2/2)
+	mesh2.material = mat
+	mesh2.flip_faces = true
+	add_child(mesh2)
+
+
+
+
+
+
+
+
+
+
+
+
